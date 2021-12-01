62dd8766f7d48673c344de9934f5cdd6a02b0bd6
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2021-12-01 19:30:49 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello world. This app works, see https://github.com/nicolas-van/rust-cross-compile-example");
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello world. This app works, see https://github.com/nicolas-van/rust-cross-compile-example".into()))
+}
+
+#[tokio::main]
+async fn main() {
+    println!("Launching web server on 127.0.0.1:3000");
+    // We'll bind to 127.0.0.1:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
